588d589f789ffa0cfeac514cb01f6370a15f9294
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-03-27 16:31:37 +0200

    added extra info to movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,14 @@ const MovieList = (props) => {
     return (
         <>
             {props.movies.map((movie) => (
-                <div className="">
-                    <img className="" src={movie.Poster} alt="movie"></img>
-                    <div className="">
-                        <h1>{props.heading}</h1>
+                <div className="movie" key={movie.imdbID}>
+                    <img className="movie-poster" src={movie.Poster} alt="movie"></img>
+                    <div className="movie-details">
+                        <h2 className="movie-title">{movie.Title}</h2>
+                        <p className="movie-year"><strong>Year:</strong> {movie.Year}</p>
+                        <p className="movie-genre"><strong>Genre:</strong> {movie.Genre}</p>
+                        <p className="movie-director"><strong>Director:</strong> {movie.Director}</p>
+                        <p className="movie-actors"><strong>Actors:</strong> {movie.Actors}</p>
                     </div>
                 </div>
             ))}
